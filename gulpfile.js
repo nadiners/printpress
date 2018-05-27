948e8ae2c0981d62968629c9361b29515aba4ead
Nadine Rotem-Stibbe <nadine@nadines-macbook-pro-2.home>
2018-05-27 15:11:23 +0200

    new branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),  
-	sourcemaps = require('gulp-sourcemaps')
-  	pug = require('gulp-pug');
+    sourcemaps = require('gulp-sourcemaps'),
+  	pug = require('gulp-pug'),
+    sass = require('gulp-sass');
 
 // run this task by typing in gulp pug in CLI
 gulp.task('pug', function() {  
@@ -14,9 +15,23 @@ gulp.task('pug', function() {
       .pipe(gulp.dest('public')); // tell gulp our output folder
 });
 
+// sass
+gulp.task('scss', function() {  
+  return gulp.src('scss/**/*.scss')
+      .on('error', function (err) {
+            console.log(err.toString());
+            this.emit('end');
+       })
+      .pipe(sourcemaps.write())
+      .pipe(sass({outputStyle: 'compressed'}))
+      .pipe(gulp.dest('public/assets/css')); // tell gulp our output folder
+});
+
+
 gulp.task('default', ['watch']);
 
 gulp.task('watch', function(){
 	gulp.watch('views/**/*.pug', ['pug']);
+  gulp.watch('scss/**/*.scss', ['scss']);
 });
 
